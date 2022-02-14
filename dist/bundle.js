2ee84a5fe0e74f0008df5be69670e767788e6d62
taylor12138 <277421392@qq.com>
2022-02-14 20:10:29 +0800

    fix:package.json的一些配置

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,14 @@
-var e={d:(r,o)=>{for(var t in o)e.o(o,t)&&!e.o(r,t)&&Object.defineProperty(r,t,{enumerable:!0,get:o[t]})},o:(e,r)=>Object.prototype.hasOwnProperty.call(e,r)},r={};e.d(r,{I:()=>o});const o=(e,r)=>e+r;var t=r.I;export{t as add};
+var e = {
+    d: (r, o) => {
+      for (var t in o)
+        e.o(o, t) &&
+          !e.o(r, t) &&
+          Object.defineProperty(r, t, { enumerable: !0, get: o[t] });
+    },
+    o: (e, r) => Object.prototype.hasOwnProperty.call(e, r),
+  },
+  r = {};
+e.d(r, { I: () => o });
+const o = (e, r) => e + r;
+var t = r.I;
+export { t as add };
