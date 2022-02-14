e434594a4233cb4bcfff8e0d189d1f306bf9405b
taylor12138 <277421392@qq.com>
2022-02-14 17:57:23 +0800

    配置导出方式

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export const add = (a, b) => a + b;
+//当然，作为库的作者，希望自己的库能通过commonjs、script标签、amd、esmodule等形式进行引用
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
+  experiments: {
+    outputModule: true, //如果以es module的形式导出，则需要配置experiments.outputModule项
+  },
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js", //打包后的文件名
-    library: "littleLib",
+    library: {
+      // name: "littleLib",
+      type: "module",
+    },
   },
 };
